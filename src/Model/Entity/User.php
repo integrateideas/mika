c03d7302127c563ca374c90590c2542899439db7
Hriday <hriday.taneja@twinspark.co>
2017-09-07 18:47:40 +0530

    Users table password hasher

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,7 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
-
+use Cake\Auth\DefaultPasswordHasher;
 /**
  * User Entity
  *
@@ -45,4 +45,14 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+     protected function _setPassword($value){
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);    
+    }
+
+    protected function _getFullName()
+    {
+        return $this->_properties['first_name'] . '  ' .
+        $this->_properties['last_name'];
+    }
 }
